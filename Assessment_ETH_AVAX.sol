a1f2286b07c09225791e646f673b398e04240a30
Tushar Gangurde <tushargangurde859@gmail.com>
2023-06-25 23:35:26 +0530

    Assessment_ETH_AVAX.sol

--- Assessment_ETH_AVAX.sol
+++ Assessment_ETH_AVAX.sol
@@ -1,3 +1,4 @@
+ //write a smart contract that implements the require(), assert() and revert() statements.
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
@@ -11,16 +12,20 @@ contract HospitalManagement {
     mapping(uint => Patient) public patients;
     uint public patientCount;
     
+    
     function admitPatient(uint _id, string memory _name) public {
-        require(_id > 0, "Invalid patient ID");
-        require(bytes(_name).length > 0, "Invalid patient name");
+        // require statement
         require(!patients[_id].admitted, "Patient is already admitted");
+        
+        // assert statement
+        assert(bytes(_name).length > 0);
         
         patients[_id] = Patient(_id, _name, true);
         patientCount++;
     }
     
     function dischargePatient(uint _id) public {
+        // require statement
         require(patients[_id].admitted, "Patient is not admitted");
         
         patients[_id].admitted = false;
@@ -28,8 +33,10 @@ contract HospitalManagement {
     }
     
     function updatePatientName(uint _id, string memory _name) public {
-        require(patients[_id].admitted, "Patient is not admitted");
-        require(bytes(_name).length > 0, "Invalid patient name");
+        // revert statement
+        if (bytes(_name).length == 0) {
+            revert("Name cannot be empty");
+        }
         
         patients[_id].name = _name;
     }
